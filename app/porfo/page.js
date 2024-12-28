21d4b28b28ecc52581fb9ae647db4d7093f5e896
Eric Ng <airic.ng@gmail.com>
2024-12-28 18:10:32 -0500

    add package

--- app/porfo/page.js
+++ app/porfo/page.js
@@ -1,7 +1,58 @@
+import Image from "next/image";
+
 export default function Media({children}) {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            Media
+            <div>
+                <Image 
+                    className="dark:invert"
+                    src="/30anni-poster-color.png"
+                    alt="30anni"
+                    width={180}
+                    height={38}
+                    priority
+                />
+            </div>
+            <div>
+                <Image
+                    className="dark:invert"
+                    src="/ChristmasEveCelebPoster5a.png"
+                    alt="ChristmasEve"
+                    width={180}
+                    height={38}
+                    priority
+                />
+            </div>
+            <div>
+                <Image
+                    className="dark:invert"
+                    src="/Dec24Poster.png"
+                    alt="Dec24-2024"
+                    width={180}
+                    height={38}
+                    priority
+                />
+            </div>
+            <div>
+                <Image
+                    className="dark:invert"
+                    src="/promoPosterAug20-23.png"
+                    alt="promo"
+                    width={180}
+                    height={38}
+                    priority
+                />
+            </div>
+            <div>
+                <Image
+                    className="dark:invert"
+                    src="/rhandbook2024.png"
+                    alt="rhandbook2024"
+                    width={180}
+                    height={38}
+                    priority
+                />
+            </div>
         </div>
     );
 }
