dd48658cec931ebab715a86c1d40d20d605596de
Eric Ng <airic.ng@gmail.com>
2025-01-01 18:50:17 -0500

    add env

--- app/about/page.js
+++ app/about/page.js
@@ -2,9 +2,14 @@ export default function AboutPage() {
     return (
         <div className="mt-16">
             <div className="p-4">
-            <div className="text-3xl">About Me</div>
-            <div className="text-2xl">Exp</div>
-            <div className="text-2xl">Edu</div>
+                <div className="text-3xl">About Me</div>
+                <div>A software engineer who does art.  An artist who does software.  </div>
+                <div className="text-2xl">Exp</div>
+                <div>
+                    Years of frontend work with various frameworks.  Years of backend work with various languages.  
+                </div>
+                <div className="text-2xl">Edu</div>
+                <div>Bachelor and Master degrees.  </div>
             </div>
         </div>
     );
