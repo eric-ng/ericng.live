601256073984a1579e4220569aecfb5c8dd6dd54
Eric Ng <airic.ng@gmail.com>
2025-01-01 18:04:07 -0500

    add drilling

--- app/porfo/@nanoapps/nanoapps/@gsapp/app.js
+++ app/porfo/@nanoapps/nanoapps/@gsapp/app.js
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
+import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
 
 const massageVal = (key, val) => {
     if (val.match(/^https?:\/\/\w+/)) {
@@ -31,6 +32,7 @@ const getData = async () => {
 export default function App({term = ''}) {
     const [keys, setKeys] = useState([]);
     const [rows, setRows] = useState([]);
+    const [curInd, setCurInd] = useState();
     useEffect(() => {
         (async () => {
             const tmpdata = await getData();
@@ -44,34 +46,60 @@ export default function App({term = ''}) {
         })();
     }, []);
 
-    const handleDrill = () => {
-
+    const handleDrill = (newInd) => {
+        setCurInd(newInd);
     }
 
     return (
-        <div className="grid divide-y divide-slate-300">
-            {rows.filter((row) => (term != '' ? row[0].toLowerCase().indexOf(term) > -1 : true)).map((row, ind) => {
-                return (
-                    <div key={`nanoapps-rows-${ind}`} className="flex bg-slate-100 shadow p-4">
-                        <div className="grow">
-                            <div className="text-2xl font-sans">{row[0]}</div>
-                            {keys.map((key, ind) => {
-                                if (ind === 0) {
-                                    return null;
-                                }
-                                return (
-                                    <div key={key}>{
-                                        massageVal(key, row[ind])
-                                    }</div>
-                                );
-                            })}
-                        </div>
-                        <div className="place-content-center px-2 hover:bg-slate-200 cursor-pointer" onClick={handleDrill}>
-                            <ArrowForwardIosOutlinedIcon />
-                        </div>
+        <>
+            {curInd == undefined && 
+                <div className="grid divide-y divide-slate-300">
+                    {rows.filter((row) => (term != '' ? row[0].toLowerCase().indexOf(term) > -1 : true)).map((row, rind) => {
+                        return (
+                            <div key={`nanoapps-rows-${rind}`} className="flex bg-slate-100 shadow p-4">
+                                <div className="grow">
+                                    <div className="text-2xl font-sans">{row[0]}</div>
+                                    {keys.map((key, ind) => {
+                                        if (ind === 0 || ind > 2) {
+                                            return null;
+                                        }
+                                        return (
+                                            <div key={key}>{
+                                                massageVal(key, row[ind])
+                                            }</div>
+                                        );
+                                    })}
+                                </div>
+                                <div className="place-content-center px-2 hover:bg-slate-200 cursor-pointer" onClick={() => handleDrill(rind)}>
+                                    <ArrowForwardIosOutlinedIcon />
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            }
+            {curInd !=undefined &&
+                <div className="grid">
+                    <div className="my-2 cursor-pointer" onClick={() => handleDrill(undefined)}>
+                        <ArrowBackIosNewOutlinedIcon />
                     </div>
-                );
-            })}
-        </div>
+                    <div className="text-[3rem]">
+                        {rows[curInd][0]}
+                    </div>
+                    <div>
+                        {keys.map((key, ind) => {
+                            if (ind === 0) {
+                                return null;
+                            }
+                            return (
+                                <div key={key}>{
+                                    massageVal(key, rows[curInd][ind])
+                                }</div>
+                            );
+                        })}
+                    </div>
+                </div>
+            }
+        </>
     );
 }
