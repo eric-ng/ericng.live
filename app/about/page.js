c58dccc06fa414f355f0ef63ec0a042811b1a9f2
Eric Ng <airic.ng@gmail.com>
2025-01-03 15:43:40 -0500

    slight update to style

--- app/about/page.js
+++ app/about/page.js
@@ -1,15 +1,19 @@
 export default function AboutPage() {
     return (
-        <div className="mt-16">
-            <div className="p-4">
+        <div className="mt-12">
+            <div className="p-4 pt-8 bg-slate-100 h-screen">
                 <div className="text-3xl">About Me</div>
-                <div>A software engineer who does art.  An artist who does software.  </div>
-                <div className="text-2xl">Exp</div>
+                <div>
+                    A software engineer who does art.  An artist who does software.  
+                </div>
+                <div className="text-2xl mt-2">Exp</div>
                 <div>
                     Years of frontend work with various frameworks.  Years of backend work with various languages.  
                 </div>
-                <div className="text-2xl">Edu</div>
-                <div>Bachelor and Master degrees.  </div>
+                <div className="text-2xl mt-2">Edu</div>
+                <div>
+                    Bachelor and Master degrees.  
+                </div>
             </div>
         </div>
     );
